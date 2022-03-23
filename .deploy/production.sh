cf80f28c065d3ae71e842a71ac7c47868a2e0c2f
Mateusz Russak <mateusz.r@masterborn.com>
2022-03-23 21:27:09 +0000

    chore: deploy

--- .deploy/production.sh
+++ .deploy/production.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 echo $(pwd)
-sudo docker run --rm -m .:/mnt node:17-alpine "cd /mnt && npm i"
+sudo docker run --rm -v "$(pwd):/mnt" node:17-alpine "cd /mnt && npm i"
 sudo docker stack deploy --compose-file=$(pwd)/docker-compose.prod.yml Chat
