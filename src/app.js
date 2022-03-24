1b76ce90ac6b596c9cf910c1eda5a790ad1411fe
Mateusz Russak <mateusz.r@masterborn.com>
2022-03-24 18:08:16 +0000

    chore: deploy

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('public'))
+app.use((req,res,next) => console.log(req.url) || next());
+app.use(express.static('public'));
 
 module.exports = app;
