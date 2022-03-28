910e9f6a148df957ac264a161f31ce8afbfd2dac
Mateusz Russak <mateusz.r@masterborn.com>
2022-03-28 23:22:28 +0200

    chore: removed unused file

--- src/push/pushRepository.js
+++
@@ -1,9 +0,0 @@
-const {knex} = require('../database/db');
-
-module.exports = {
-  getOther: async ({userId}) => knex('push').select().where('userId', '!=', userId),
-  upsert: async (msg) => knex('push').insert(msg)
-    .onConflict(['userId', 'sessionId'])
-    .merge()
-    .returning('*'),
-}
