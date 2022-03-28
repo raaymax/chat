0b67da837b9721357476c7ddbf246c464ec5dd72
Mateusz Russak <mateusz.r@masterborn.com>
2022-03-28 23:18:48 +0200

    chore: cleanup

--- public/sw.js
+++ public/sw.js
@@ -69,8 +69,6 @@ self.addEventListener('notificationclick', function(event) {
   console.log('On notification click: ', event.notification.tag);
   event.notification.close();
 
-  // This looks to see if the current is already open and
-  // focuses if it is
   event.waitUntil(clients.matchAll({
     type: "window"
   }).then(function(clientList) {
