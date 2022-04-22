2130ca79cf2fee8e43ae269039e9d8668f3aa43c
Mateusz Russak <mateusz.r@masterborn.com>
2022-04-22 11:06:52 +0200

    fix: build and deploy

--- packages/server/src/app.js
+++ packages/server/src/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.set('trust proxy', true);
+
 // eslint-disable-next-line no-console
 // app.use((req, res, next) => console.log(req.url) || next());
 app.use(express.static('../app/dist'));
