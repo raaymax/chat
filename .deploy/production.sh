e2113ed1e2db777c9022e159d803348596502d68
Mateusz Russak <mateusz.r@masterborn.com>
2022-05-11 15:23:25 +0200

    fix: deploy

--- .deploy/production.sh
+++ .deploy/production.sh
@@ -3,5 +3,5 @@
 APP_VERSION="$(cat package.json | awk '/version/{print $2}' | sed 's/[",]//g')"
 sudo cp ../chat.config.js ./
 gcloud builds submit --tag gcr.io/codecat-quack/quack:${APP_VERSION}
-sudo env APP_VERSION=${APP_VERSION} COMMIT_HASH=${COMMIT_HASH} docker stack deploy --compose-file=$(pwd)/docker-compose.prod.yml Chat
+sudo env APP_VERSION=${APP_VERSION} COMMIT_HASH=${COMMIT_HASH} docker stack deploy --with-registry-auth --compose-file=$(pwd)/docker-compose.prod.yml Chat
 sudo docker image prune -f
