3b02e2a8ddeb37148a7eb83484b26a288593a4a7
Mateusz Russak <mateusz.r@masterborn.com>
2022-05-22 19:57:25 +0200

    feat: prompt ai command

--- packages/server/src/ai/aiController.js
+++ packages/server/src/ai/aiController.js
@@ -10,7 +10,55 @@ function initAI() {
     });
     const openai = new OpenAIApi(configuration);
     return {
-      createCompletion: async (self, msg) => {
+      prompt: async (self, msg) => {
+        if (!self.user) return msg.error(Errors.AccessDenied());
+        await msg.ok();
+
+        const prompt = msg.flat.replace(/^\/prompt /, '');
+
+        const args = {
+          prompt,
+          temperature: 0.7,
+          max_tokens: 256,
+        };
+
+        const { data } = await openai.createCompletion('text-davinci-002', args);
+        const resp = {
+          user: {
+            id: 'openai',
+            name: 'OpenAI',
+            avatarUrl: '/assets/openai.png',
+          },
+          message: [
+            {
+              line: [
+                { bold: { text: msg.user.name } },
+                { text: ' asked: "' },
+                { italic: { text: prompt } },
+                { text: '"' }],
+            },
+            ...data.choices[0].text.split('\n').map((line) => ({
+              line: { text: line },
+            })).filter((l) => !!l.line.text),
+          ],
+          metadata: {
+            openai: {
+              request: {
+                model: 'text-davinci-002',
+                ...args,
+              },
+              response: data,
+            },
+          },
+          flat: `${prompt}: ${data.choices[0].text}`,
+          createdAt: new Date(),
+          channel: msg.channel,
+        };
+        const { id } = await messageRepo.insert(resp);
+        resp.id = id;
+        await self.broadcast(resp);
+      },
+      ai: async (self, msg) => {
         if (!self.user) return msg.error(Errors.AccessDenied());
         await msg.ok();
 
@@ -68,7 +116,8 @@ function initAI() {
     };
   }
   return {
-    createCompletion: (self, msg) => { msg.error(Errors.FeatureNotEnabled()); },
+    prompt: (self, msg) => { msg.error(Errors.FeatureNotEnabled()); },
+    ai: (self, msg) => { msg.error(Errors.FeatureNotEnabled()); },
   };
 }
 module.exports = initAI();
