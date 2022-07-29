754533b5e567d51b09bf8f2f64a43851b5ee9e42
Mateusz Russak <mateusz.r@masterborn.com>
2022-07-29 14:00:29 +0200

    fix: migrations part 2

--- migrations/20220626150314-alter-messages-add-unique-index.js
+++ migrations/20220626150314-alter-messages-add-unique-index.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-restricted-syntax */
 module.exports = {
   async up(db) {
-    const cursor = await db.collection('messages').aggregate([{ $group: { _id: '$clientId', count: { $sum: 1 } } }, { $match: { count: { $gt: 1 } } }]);
+    const cursor = await db.collection('messages').find({});
     for await (const fixItem of cursor) {
       await db.collection('messages').updateOne(
         { clientId: { $eq: fixItem._id } },
