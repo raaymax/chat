079f4074b08f3726fba266f15cc7c329a32ebf7d
Mateusz Russak <mateusz.r@masterborn.com>
2022-07-29 14:31:16 +0200

    fix: deploy conditions + migrations

--- migrations/20220626150314-alter-messages-add-unique-index.js
+++ migrations/20220626150314-alter-messages-add-unique-index.js
@@ -3,13 +3,14 @@ module.exports = {
   async up(db) {
     const cursor = await db.collection('messages').find({});
     for await (const fixItem of cursor) {
+      console.log(fixItem._id);
       await db.collection('messages').updateOne(
-        { clientId: { $eq: fixItem._id } },
-        [{
+        { clientId: fixItem._id },
+        {
           $set: {
             clientId: `temp:${Math.random() + 1}`,
           },
-        }],
+        },
       );
     }
     return db.collection('messages').createIndex({ clientId: 1 }, { unique: true });
