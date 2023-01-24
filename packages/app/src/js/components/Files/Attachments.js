2115baaeda345e31f049fde733966fc2f9dbeb2a
Mateusz Russak <mateusz.r@masterborn.com>
2023-01-24 12:36:43 +0800

    v1.6.2

--- packages/app/src/js/components/Files/Attachments.js
+++ packages/app/src/js/components/Files/Attachments.js
@@ -1,7 +1,58 @@
 import { h } from 'preact';
 import { useDispatch, useSelector } from 'react-redux';
+import styled from 'styled-components';
 import { abort } from '../../services/file';
 import { selectors } from '../../state';
+
+const Container = styled.div`
+  .attachment {
+    position: relative;
+    height: 30px;
+    padding: 0;
+    margin: 3px 15px;
+    display: flex;
+    flex-direction: row;
+    border: 1px solid var(--saf-0);
+    max-width: calc(100vw - 30px);
+
+    .type {
+      line-height: 30px;
+      width: 30px;
+      text-align: center;
+      vertical-align: middle;
+    }
+
+    .name {
+      padding: 0 10px;
+      line-height: 30px;
+      vertical-align: middle;
+      flex: 1;
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+
+    .remove {
+      color: #852007;
+      cursor: pointer;
+      line-height: 30px;
+      width: 30px;
+      text-align: center;
+      vertical-align: middle;
+      &:hover {
+        background-color: var(--primary_active_mask);
+      }
+    }
+
+    .progress {
+      background-color: green;
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      height: 3px;
+    }
+  }
+`;
 
 export const Attachment = ({data: {fileName, contentType, progress}, ondelete}) => (
   <div class='attachment'>
@@ -17,7 +68,7 @@ export const Attachments = () => {
   const dispatch = useDispatch();
 
   return (
-    <div>
+    <Container>
       {list.map((file) => (
         <Attachment
           key={file.clientId}
@@ -27,6 +78,6 @@ export const Attachments = () => {
           }}
         />
       ))}
-    </div>
+    </Container>
   );
 }
--- packages/app/src/js/components/Files/Files.js
+++ packages/app/src/js/components/Files/Files.js
@@ -1,4 +1,5 @@
 import { h } from 'preact';
+import styled from 'styled-components';
 import { getUrl, getThumbnail } from '../../services/file';
 
 const download = async (fileId) => {
@@ -7,6 +8,56 @@ const download = async (fileId) => {
 
 const IMAGE_TYPES = ['image/png', 'image/jpeg', 'image/jpg', 'image/gif', 'image/webp'];
 const RAW_IMAGE_TYPES = ['image/gif', 'image/webp'];
+
+const Container = styled.div`
+  max-width: calc(100vw - 92px);
+  .file-list {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+  }
+
+  .file {
+    cursor: pointer;
+    flex: 100%;
+    width: 100%;
+    height: 30px;
+    padding: 0;
+    margin: 3px 0px;
+    display: flex;
+    flex-direction: row;
+    border: 1px solid var(--saf-0);
+    .type {
+      line-height: 30px;
+      width: 30px;
+      text-align: center;
+      vertical-align: middle;
+    }
+
+   .name {
+      padding: 0 10px;
+      line-height: 30px;
+      vertical-align: middle;
+      flex: 1;
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+    &:hover {
+      background-color: var(--primary_active_mask);
+    }
+    &.image {
+      height: auto;
+      width: 100%;
+      flex: 0;
+
+      img.raw-image {
+        max-width: 400px;
+        max-height: 400px;
+      }
+    }
+  }
+`;
 
 export const File = ({data: {fileName, contentType, id}}) => (
   <div class='file' data-id={id} onclick={() => download(id)}>
@@ -26,7 +77,7 @@ export const Image = ({raw, data: {fileName, id}}) => (
 )
 
 export const Files = ({list}) => (
-  <div>
+  <Container>
     <div class='file-list'>
       {list
         .filter((file) => IMAGE_TYPES.includes(file.contentType))
@@ -42,5 +93,5 @@ export const Files = ({list}) => (
         .filter((file) => !IMAGE_TYPES.includes(file.contentType))
         .map((file) => <File key={file.clientId} data={file} />)}
     </div>
-  </div>
+  </Container>
 )
