3864c788264154d90b712fa6b4db8b2f7591c13d
Mateusz Russak <mateusz.r@masterborn.com>
2023-05-04 22:26:08 +0200

    fix: linter errors

--- config/index.js
+++ config/index.js
@@ -1,10 +1,12 @@
+/* eslint-disable global-require */
 module.exports = (() => {
   try {
     if (process.env.NODE_ENV === 'test') {
-      return require('../tests/chat.config.tests.js');
+      return require('../tests/chat.config.tests');
     }
-    return require('../chat.config.js');
+    return require('../chat.config');
   } catch (err) {
+    // eslint-disable-next-line no-console
     console.error('Config file is missing');
     process.exit(1);
   }
