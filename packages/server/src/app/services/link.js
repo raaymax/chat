7eba5309fd9f118403879a02bb52ca635aac5698
Mateusz Russak <mateusz.r@masterborn.com>
2023-05-20 17:57:04 +0200

    fix(links): do not display links when no preview

--- packages/server/src/app/services/link.js
+++ packages/server/src/app/services/link.js
@@ -5,7 +5,8 @@ module.exports = {
   addPreview: async ({ messageId, links }, { bus }) => {
     try {
       const data = await Promise.all(links.map((link) => getLinkPreview(link)));
-      await repo.message.update({ id: messageId }, { linkPreviews: data });
+      const prev = data.filter((link) => link.images?.length > 0 || link.description?.length > 0);
+      await repo.message.update({ id: messageId }, { linkPreviews: prev });
       const updated = await repo.message.get({ id: messageId });
       await bus.broadcast({ type: 'message', ...updated });
     } catch (err) {
