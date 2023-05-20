173e661d7b87d1802f32a49a4cabadfe3f51869c
Mateusz Russak <mateusz.r@masterborn.com>
2023-05-20 11:47:56 +0200

    fix(mobile): keyboard is jumping

--- packages/app/src/js/pages/app.js
+++ packages/app/src/js/pages/app.js
@@ -2,18 +2,19 @@ import { h, render } from 'preact';
 import { Suspense, lazy } from 'preact/compat';
 import '../core/registerSw';
 import { Login } from './login';
-import { VisibleViewport } from '../components/VisibleViewport/VisibleViewport';
 
 const Secured = lazy(() => import('./secured'));
 
+if ('virtualKeyboard' in navigator) {
+  navigator.virtualKeyboard.overlaysContent = true;
+}
+
 export const App = () => (
-  <VisibleViewport>
-    <Login>
-      <Suspense fallback={<div>loading secured page...</div>}>
-        <Secured />
-      </Suspense>
-    </Login>
-  </VisibleViewport>
+  <Login>
+    <Suspense fallback={<div>loading secured page...</div>}>
+      <Secured />
+    </Suspense>
+  </Login>
 )
 
 render(<App />, document.getElementById('root'));
