d3af841706f426d53186d8c7284ab0cafa63499e
Mateusz Russak <mateusz.r@masterborn.com>
2023-06-20 15:25:09 +0200

    fix: user mentions redirect to direct channel

--- packages/app/src/js/components/UserInline/UserInline.js
+++ packages/app/src/js/components/UserInline/UserInline.js
@@ -1,19 +1,22 @@
 import { h } from 'preact';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import styled from 'styled-components';
 import { selectors } from '../../state';
+import { gotoDirectChannel } from '../../services/channels';
 
-const InlineUserLink = styled.span`
+const InlineUserLink = styled.a`
   span {
     padding-left: 1px;
-    color: ${props => props.theme.mentionsColor};
+    color: ${(props) => props.theme.mentionsColor};
   }
 `;
 
 export const UserInline = ({ userId: id }) => {
+  const dispatch = useDispatch();
   const user = useSelector(selectors.getUser(id));
+
   return (
-    <InlineUserLink className='channel' data-id={id} href={`#`} >
+    <InlineUserLink className='channel' onClick={() => dispatch(gotoDirectChannel(id))} data-id={id} href={`#`} >
       <span class='name'>@{user?.name || id}</span>
     </InlineUserLink>
   );
