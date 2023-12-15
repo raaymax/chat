e5b31f27cfb99c72c91d05429335099ef1611cff
Mateusz Russak <mateusz.r@masterborn.com>
2023-12-15 21:39:08 +0100

    fix: migrations

--- migrations/20220509210354-create-default-users.js
+++ migrations/20220509210354-create-default-users.js
@@ -8,7 +8,7 @@ module.exports = {
     if (await db.collection('users').countDocuments() === 0) {
       await db.collection('users').insertMany([
         {
-          _id: ObjectId('6255a4156c28443c92c26d7e'),
+          _id: new ObjectId('6255a4156c28443c92c26d7e'),
           clientId: '7ed5c52c-35f8-4a27-929d-ff5eb1a74924',
           login: 'admin',
           password: genHash('123'),
@@ -16,7 +16,7 @@ module.exports = {
           avatarUrl: '',
         },
         {
-          _id: ObjectId('6255a4156c28443c92c26d7d'),
+          _id: new ObjectId('6255a4156c28443c92c26d7d'),
           clientId: 'c3875674-61f1-4793-a558-a733293f3527',
           login: 'member',
           password: genHash('123'),
