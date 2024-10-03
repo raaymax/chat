9455b4623a281ec1c653cb97169700a16a03f792
Mateusz Russak <mateusz@russak.biz>
2024-10-03 12:30:09 +0200

    fix: sidebar crash when unknown channel is in url

--- app/src/js/store/hooks/useBadges.ts
+++ app/src/js/store/hooks/useBadges.ts
@@ -4,6 +4,7 @@ import { useSelector } from './useSelector';
 export const useBadges = (userId: string | null | undefined) => {
   const progress = useSelector((state) => state.progress);
   return useMemo(() => progress
+    .filter((p) => Boolean(p))
     .filter((p) => p.userId === userId)
     .filter((p) => !p.parentId)
     .reduce((acc, p) => ({
